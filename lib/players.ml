218ada1c466f4138a30932485642f9a2c4b6329a
francis <fpham0701@gmail.com>
2024-11-28 17:41:33 -0500

    Updated players compilation unit
    - modulated some functions from to players
    - added more helper functions to give access to other lib files
    - updated abstract type to have contribution and cards

--- lib/players.ml
+++ lib/players.ml
@@ -2,52 +2,26 @@ open Cards
 
 type t = {
   name : string;
-  cards : Cards.t * Cards.t;
+  mutable cards : Cards.t * Cards.t;
+  mutable contribution : int;
 }
-
-let deck = Cards.create_deck ()
 
 let get_name player = player.name
 let get_card player = player.cards
-
-let create_players () =
-  let player_names =
-    [
-      "Player 1";
-      "Player 2";
-      "Player 3";
-      "Player 4";
-      "Player 5";
-      "Player 6";
-      "Player 7";
-      "Player 8";
-      "Player 9";
-    ]
-  in
-  List.map
-    (fun name ->
-      let player_cards = 
-        let drawn1 = Cards.draw deck in
-        let drawn2 = Cards.draw (snd drawn1) in
-        (fst drawn1, fst drawn2)
-      in
-      { name; cards = player_cards })
-    player_names
+let get_contribution player = player.contribution
 
 let create_players_with_names names =
-  let rec aux acc = function
-    | [] -> List.rev acc
+  let rec create_players names =
+    match names with
+    | [] -> []
     | name :: rest ->
-        let card1, card2 = Cards.random_two_cards () in
-        aux ({ name; cards = (card1, card2) } :: acc) rest
+        let cards = Cards.random_two_cards () in
+        { name; cards; contribution = 0 } :: create_players rest
   in
-  aux [] names
+  create_players names
 
 let print_player player =
   Printf.printf "Player: %s\nCards: %s\n" player.name
-    (Printf.sprintf "%s, %s"
-       (Cards.string_of_card (fst player.cards))
-       (Cards.string_of_card (snd player.cards)))
-
-let remove_player player player_list =
-  List.filter (fun x -> x.name <> player.name) player_list
+    (Cards.string_of_card (fst player.cards)
+    ^ ", "
+    ^ Cards.string_of_card (snd player.cards))
--- lib/players.mli
+++ lib/players.mli
@@ -1,24 +1,21 @@
-type t
+open Cards
+
+type t = {
+  name : string;
+  mutable cards : Cards.t * Cards.t;
+  mutable contribution : int;
+}
 (** Abstract type representing a player and their cards *)
 
 val get_name : t -> string
 (** [get_name player] returns the name of [player]. *)
 
 val get_card : t -> Cards.t * Cards.t
-(** [get_name card] returns the card of [player]. *)
-
-val create_players : unit -> t list
-(** [create_players ()] creates a list of default players with names "Player 1",
-    "Player 2", "Player 3", and "Player 4", each with two random cards. *)
+(** [get_card player] returns the cards of [player]. *)
 
 val create_players_with_names : string list -> t list
 (** [create_players_with_names names] creates a list of players with the given
     [names], each player receiving two random cards. *)
 
 val print_player : t -> unit
-(** [print_player player] prints the name and cards of [player] in a readable
-    format. *)
-
-val remove_player : t -> t list -> t list
-(** [remove_player player player_list] removes the given [player] from the
-    [player_list]. *)
+(** [print_player player] prints the name and cards of [player]. *)

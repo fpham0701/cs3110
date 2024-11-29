afa608ce98eba3fb79ec7c9b25cae9e3e325911d
francis <fpham0701@gmail.com>
2024-11-28 20:55:19 -0500

    Added functionality to print out more
    - prints out current pot and current bet for each round
    - added more functionality to match the actions

--- lib/actions.ml
+++ lib/actions.ml
@@ -8,18 +8,26 @@ type t =
   | Raise of int
 
 let action player state options =
-  print_endline "\n===--------------------------------===";
-  Printf.printf "           Current Pot: %i           \n" (get_pot state);
-  print_endline "===--------------------------------===";
+  print_endline "===-----------------------------------===";
+  Printf.printf "             Current Pot: %i           \n" (get_pot state);
+  Printf.printf "             Current Bet: %i           \n"
+    (get_current_bet state);
+  print_endline "===-----------------------------------===";
 
   Printf.printf "\nIt is %s's turn to go!\n" (get_name player);
-  print_endline ("Available actions: " ^ String.concat ", " options);
-  print_endline "Enter your action:";
   let rec prompt_action () =
+    print_endline ("Available actions: " ^ String.concat ", " options);
+    print_endline "Enter your action:";
     let input = read_line () in
     match input with
     | "call" when List.mem "call" options -> Call
-    | "check" when List.mem "check" options -> Check
+    | "check"
+      when List.mem "check" options
+           && get_contributions player = get_current_bet state -> Check
+    | "check" ->
+        print_endline
+          "You can only check if your contribution matches the current pot.";
+        prompt_action ()
     | "fold" when List.mem "fold" options -> Fold
     | "raise" when List.mem "raise" options ->
         print_endline "Enter the amount to raise:";

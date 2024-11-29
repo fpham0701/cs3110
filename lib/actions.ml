3e14fabd905579c8270fcd5d61af998e7708d58e
francis <fpham0701@gmail.com>
2024-11-28 19:37:37 -0500

    Refactored actions compilation unit
    - removed unnecessary functions and reallocated to other lib files
    - action now maintains the function to prompt user for action input
    - cleaned up documentation

--- lib/actions.ml
+++ lib/actions.ml
@@ -1,83 +1,32 @@
+open Players
+open State
+
 type t =
   | Call
   | Check
   | Fold
   | Raise of int
-      (** Abstract type representing a player's action in a poker game. *)
 
-(** Represents the action of calling the current bet in the game. *)
-let call = Call
+let action player state options =
+  print_endline "\n===--------------------------------===";
+  Printf.printf "           Current Pot: %i           \n" (get_pot state);
+  print_endline "===--------------------------------===";
 
-(** Represents the action of checking, meaning the player chooses not to bet if
-    no other bet has been made. *)
-let check = Check
-
-(** Represents the action of folding, meaning the player gives up their hand and
-    forfeits the round. *)
-let fold = Fold
-
-(** Represents the action of raising, where a player increases the stakes by
-    betting more. *)
-let raise amount = Raise amount
-
-(** Prompts the user to choose an action from the available options (call,
-    check, fold, or raise). *)
-let action options =
-  let rec get_choice () =
-    print_endline "Choose an action:";
-    List.iteri (fun i option -> Printf.printf "%d. %s\n" (i + 1) option) options;
-    match read_line () with
-    | input -> (
-        try
-          let choice = int_of_string input in
-          if choice > 0 && choice <= List.length options then (
-            match List.nth options (choice - 1) with
-            | "call" -> call
-            | "check" -> check
-            | "fold" -> fold
-            | "raise" ->
-                print_endline "Enter the amount to raise:";
-                let amount = int_of_string (read_line ()) in
-                raise amount
-            | _ ->
-                print_endline "Invalid choice. Please try again.";
-                get_choice ())
-          else (
-            print_endline "Invalid choice. Please try again.";
-            get_choice ())
-        with Failure _ ->
-          print_endline "Invalid input. Please enter a number.";
-          get_choice ())
-  in
-  get_choice ()
-
-(** Allows the user to specify the size of their bet by choosing from preset
-    options or entering a custom size. Returns the chosen bet size. *)
-let bet_size current_bet =
-  let rec choose_size () =
-    print_endline "Choose the bet size:";
-    print_endline
-      "1. Twice (2x) the current bet\n\
-       2. Three times (3x) the current bet\n\
-       3. Four times (4x) the current bet\n\
-       4. Pot size\n\
-       5. All-in\n\
-       6. Custom size";
-    match read_line () with
-    | "1" -> current_bet * 2
-    | "2" -> current_bet * 3
-    | "3" -> current_bet * 4
-    | "4" ->
-        print_endline "Enter the pot size:";
-        int_of_string (read_line ())
-    | "5" ->
-        print_endline "Enter the all-in amount:";
-        int_of_string (read_line ())
-    | "6" ->
-        print_endline "Enter your custom bet size:";
-        int_of_string (read_line ())
+  Printf.printf "\nIt is %s's turn to go!\n" (get_name player);
+  print_endline ("Available actions: " ^ String.concat ", " options);
+  print_endline "Enter your action:";
+  let rec prompt_action () =
+    let input = read_line () in
+    match input with
+    | "call" when List.mem "call" options -> Call
+    | "check" when List.mem "check" options -> Check
+    | "fold" when List.mem "fold" options -> Fold
+    | "raise" when List.mem "raise" options ->
+        print_endline "Enter the amount to raise:";
+        let amount = int_of_string (read_line ()) in
+        Raise amount
     | _ ->
-        print_endline "Invalid choice. Please try again.";
-        choose_size ()
+        print_endline "Invalid action. Please try again.";
+        prompt_action ()
   in
-  choose_size ()
+  prompt_action ()
--- lib/actions.mli
+++ lib/actions.mli
@@ -1,27 +1,11 @@
-type t
 (** Abstract type representing a player's action in a poker game *)
+type t =
+  | Call
+  | Check
+  | Fold
+  | Raise of int
 
-val action : string list -> t
-(** [action options] prompts the user to choose an action from the available
-    [options] and calls the function (call, check, fold, or raise). *)
-
-val call : t
-(** [call] represents the action of calling the current bet in the game. *)
-
-val check : t
-(** [check] represents the action of checking, meaning the player chooses not to
-    bet if no other bet has been made. *)
-
-val fold : t
-(** [fold] represents the action of folding, meaning the player gives up their
-    hand and forfeits the round. *)
-
-val raise : int -> t
-(** [raise amount] represents the action of raising the current bet by [amount],
-    meaning the player increases the stakes by betting more. *)
-
-val bet_size : int -> int
-(** [bet_size amount] allows the user to specify the size of their bet by
-    providing an entering how much they want to bet with the options twice (2x)
-    the size of the bet, three times (3x), four times (4x), pot, all-in or a
-    custom size. The function returns the chosen bet size. *)
+val action : Players.t -> State.t -> string list -> t
+(** [action options] prompts the user to choose an action for [player] for the
+    current game [state] from the available [options] and calls the function
+    (call, check, fold, or raise). *)

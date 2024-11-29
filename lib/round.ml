1f03e83d8fb88d3472d7c1f40ab5fbf20c681e66
francis <fpham0701@gmail.com>
2024-11-28 20:21:15 -0500

    Refactored round to condense and better code quality
    - Turned a repeating code body to a function for multiple calls

--- lib/round.ml
+++ lib/round.ml
@@ -3,7 +3,7 @@ open Cards
 open Actions
 open State
 
-(** [split_at n] splits the list at index [n] *)
+(** [split_at n] is a helper function that splits a [lst] at index [n] *)
 let split_at n lst =
   let rec aux i acc = function
     | [] -> (List.rev acc, [])
@@ -11,6 +11,21 @@ let split_at n lst =
     | xs -> (List.rev acc, xs)
   in
   aux 0 [] lst
+
+let bet_round state =
+  reset_current_bet state;
+  List.iter (fun player -> set_contributions player 0) (get_players state);
+  List.iter
+    (fun player ->
+      print_endline ("It's " ^ get_name player ^ "'s turn:");
+      let options = [ "call"; "check"; "fold"; "raise" ] in
+      let action = action player state options in
+      match action with
+      | Call -> call state player (get_current_bet state)
+      | Check -> check state player
+      | Fold -> fold state player
+      | Raise amount -> raise_bet state player amount)
+    (get_players state)
 
 let start_game player_names =
   let players = create_players_with_names (List.rev player_names) in
@@ -20,41 +35,48 @@ let game_loop state =
   let active_players = get_players state in
   if List.length active_players <= 1 then
     match active_players with
-    | head :: [] ->
-        Printf.printf "%s is the winner!" (Players.get_name head);
+    | [ winner ] ->
+        Printf.printf "%s is the winner!\n" (get_name winner);
         print_endline "Game over!"
     | _ -> print_endline "Game over!"
-  else
-    (* Betting phase before community cards *)
-    List.iter
-      (fun player ->
-        let options = [ "call"; "check"; "fold"; "raise" ] in
-        let action = action player state options in
-        match action with
-        | Call -> call state player (get_pot state)
-        | Check -> check state player
-        | Fold -> fold state player
-        | Raise amount -> raise_bet state player amount)
-      (get_players state);
+  else (*First betting round before community cards *)
+    bet_round state;
 
   (* Reveal the flop (first 3 community cards) *)
   let deck = create_deck () in
-  let flop, _ = split_at 3 deck in
-  (* Split the deck into first 3 cards and remaining *)
+  let flop, remaining_deck = split_at 3 deck in
   set_community_cards state flop;
-  print_endline "\n=====================================";
-  Printf.printf "          Flop: %s\n"
+  print_endline ("\n" ^ String.make 50 '\n');
+  print_endline "=========================================";
+  Printf.printf "            Flop: %s\n"
     (String.concat ", " (List.map Cards.string_of_card flop));
-  print_endline "=====================================\n";
+  print_endline "=========================================\n";
 
-  (* Another betting phase after flop *)
-  List.iter
-    (fun player ->
-      let options = [ "call"; "check"; "fold"; "raise" ] in
-      let action = action player state options in
-      match action with
-      | Call -> call state player (get_pot state)
-      | Check -> ()
-      | Fold -> fold state player
-      | Raise amount -> raise_bet state player amount)
-    (get_players state)
+  (* Another betting phase after the flop *)
+  bet_round state;
+
+  (* Reveal the turn (4th community card) *)
+  let turn, remaining_deck = split_at 1 remaining_deck in
+  set_community_cards state turn;
+  print_endline ("\n" ^ String.make 50 '\n');
+  print_endline "=========================================";
+  Printf.printf "          Turn: %s\n"
+    (String.concat ", "
+       (List.map Cards.string_of_card (get_community_cards state)));
+  print_endline "=========================================\n";
+
+  (* Another betting phase after the turn *)
+  bet_round state;
+
+  (* Reveal the river (5th community card) *)
+  let river, _ = split_at 1 remaining_deck in
+  set_community_cards state river;
+  print_endline ("\n" ^ String.make 50 '\n');
+  print_endline "=========================================";
+  Printf.printf "         River: %s\n"
+    (String.concat ", "
+       (List.map Cards.string_of_card (get_community_cards state)));
+  print_endline "=========================================\n";
+
+  (* Final betting phase after the river *)
+  bet_round state
